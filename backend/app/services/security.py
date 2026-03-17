2cf8838705311b060a12b0af5843ac39090219c9
YibingW <99164336+MaggieW-code@users.noreply.github.com>
2026-03-16 21:53:57 -0700

    Update SECRET_KEY for authentication

--- backend/app/services/security.py
+++ backend/app/services/security.py
@@ -8,7 +8,7 @@ import jwt
 # security logic for feat 1
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-SECRET_KEY = "your-secret-key-change-this-later"
+SECRET_KEY = "food-delivery-auth-key"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
