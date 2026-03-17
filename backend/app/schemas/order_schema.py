b1a1de23a5bb35bc5cf9f750400bf76b3766c852
DAHLIA MOHAMMADI <mohammadi.dahlia@gmail.com>
2026-03-16 22:13:59 -0700

    Revert "Feat4-Order-Management-Schema"

--- backend/app/schemas/order_schema.py
+++ backend/app/schemas/order_schema.py
@@ -1,18 +1,17 @@
-import uuid
 from pydantic import BaseModel
 from  datetime import datetime
 
 class OrderCreate(BaseModel):
     order_id: str
     restaurant_id: int
-    customer_id: uuid
+    customer_id: str #change customer_id data type to UUID
     food_item: str
     order_qty: int
 
 class Order(BaseModel):
     order_id: str
     restaurant_id: int
-    customer_id: uuid
+    customer_id: str #change customer_id data type to UUID
     food_item: str
     order_qty: int
     order_status: str
--- backend/app/services/order_services.py
+++ backend/app/services/order_services.py
@@ -1,6 +1,7 @@
 import uuid
 from fastapi import HTTPException
 from typing import List
+
 from app.schemas.order_schema import Order, OrderCreate
 from app.repositories.orders_repo import(
     create_order,
@@ -12,7 +13,7 @@ from app.repositories.orders_repo import(
 def create_new_order(payload: OrderCreate) -> Order:
     order_data = payload.dict()
     order_data["order_id"]= str (uuid.uuid4())
-    order_data["order_status"] = "Order Created"
+    order_data["order_status"] = "created"
     created = create_order(order_data)
     return Order(**created)
 
@@ -31,6 +32,7 @@ def change_order_status(order_id: str, status: str) -> Order:
     if not updated:
         raise HTTPException(status_code = 404, detail = "Order Not Found") 
     return Order(**updated)
+
 
 
 
