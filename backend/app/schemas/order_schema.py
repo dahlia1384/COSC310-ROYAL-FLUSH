e47e76a74e8a1117820bfe7d464dca8e4b32b580
lavend02 <lavend02@student.ubc.ca>
2026-03-20 08:03:51 -0700

    Corrected: updated customer_id from str to UUID

--- backend/app/schemas/order_schema.py
+++ backend/app/schemas/order_schema.py
@@ -1,25 +1,25 @@
-import uuid
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
 from  datetime import datetime
+from uuid import UUID
 
 class OrderCreate(BaseModel):
     order_id: str
     restaurant_id: int
-    customer_id: uuid
-    food_item: str
-    order_qty: int
+    customer_id: UUID
+    food_item: str = Field(min_length=1)
+    order_qty: int = Field(gt=0)
 
 class Order(BaseModel):
     order_id: str
     restaurant_id: int
-    customer_id: uuid
+    customer_id: UUID
     food_item: str
     order_qty: int
     order_status: str
     order_time: datetime
 
 class OrderStatusUpdate(BaseModel):
-    order_status: str
+    order_status: str = Field(min_length=1)
 
 
     
